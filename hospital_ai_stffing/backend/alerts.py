788b2e53adecf6ff3d1f91db5eb83c525c15fd5f
Lalatendu Ku. Sahu <rintusahu63@gmail.com>
2026-02-04 11:49:17 +0530

    Delete hospital_ai_stffing/backend directory

--- hospital_ai_stffing/backend/alerts.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-def send_alert(staff_id, message):
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print(f"[{timestamp}] 📲 Alert sent to {staff_id}: {message}")
-
-def bulk_alert(staff_list, message):
-    for staff in staff_list:
-        send_alert(staff, message)
